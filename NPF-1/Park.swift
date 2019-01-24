5362445b5e9dbcaaad9ac9c6b6bf5e83d2f6c174
zacalmight <zacharryg15@gmail.com>
2019-01-24 14:18:57 -0500

    init

---
+++ NPF-1/Park.swift
@@ -0,0 +1,20 @@
+//
+//  Park.swift
+//  NPF-1
+//
+//  Created by Zacharry Georges on 1/24/19.
+//  Copyright © 2019 Zacharry Georges. All rights reserved.
+//
+
+import Foundation
+
+class Park{
+    private var parkName: String = " "
+    private var parkLocation: String = " "
+    private var dateFormed: String = " "
+    private var area: String = " "
+    private var link: String = " "
+    
+    func getParkName() -> String {return parkName}
+    func set(name: String) {parkName = name}
+}
